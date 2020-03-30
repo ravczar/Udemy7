3ebfeedb69e9d50f59b4c0361a8a3bf2c4745c04
ravczar <ravczar@gmail.com>
2020-03-30 15:27:07 +0200

    view projectname required info under input

--- reactiveFormsTask/src/app/app.component.ts
+++ reactiveFormsTask/src/app/app.component.ts
@@ -9,20 +9,23 @@ import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 export class AppComponent implements OnInit {
   projectStatus: Array<string>;
   assignmentForm: FormGroup;
+  defaultStatus: string;
 
   constructor(){}
 
   ngOnInit(): void{
+    // Init array of status
     this.projectStatus = new Array<string>(
       "Stable", "Critical", "Finished"
     );
-
+    // Init def. status
+    this.defaultStatus = this.projectStatus[0];
+    // Build form in TS
     this.assignmentForm = new FormGroup({
       'projectname': new FormControl(null, Validators.required),
       'email': new FormControl(null, Validators.required),
-      'projectstatus': new FormControl(null, Validators.required),
+      'projectstatus': new FormControl(this.defaultStatus, Validators.required),
     });
-
   }
 
   onSubmitForm(){
