df316ffebb8874ad9e52934ff5e13e2a22b22898
ravczar <ravczar@gmail.com>
2020-03-30 14:45:25 +0200

    add FormGroup to app.component

--- reactiveFormsTask/src/app/app.component.ts
+++ reactiveFormsTask/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactiveFormsTask';
+export class AppComponent implements OnInit {
+  projectStatus: Array<string>;
+  assignmentForm: FormGroup;
+
+  constructor(){
+    this.projectStatus = new Array<string>(
+      "Stable", "Critical", "Finished"
+    );
+  }
+
+  ngOnInit(){
+    
+  }
+
 }
